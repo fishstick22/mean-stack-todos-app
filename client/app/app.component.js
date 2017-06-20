dc8aefa5154f6f89f62401d62b8250260a51f265
Jeff <jeff.burkholder@gmail.com>
2017-06-20 10:22:23 -0500

    same complete display todos (#5)

--- client/app/app.component.js
+++ client/app/app.component.js
@@ -7,6 +7,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var todo_service_1 = require("./services/todo.service");
 var AppComponent = (function () {
     function AppComponent() {
     }
@@ -15,6 +16,7 @@ var AppComponent = (function () {
             moduleId: module.id,
             selector: 'my-app',
             templateUrl: 'app.component.html',
+            providers: [todo_service_1.TodoService]
         })
     ], AppComponent);
     return AppComponent;
