d26e3e34bee1df751eac2a6ad80de21c1e988f10
Jeff <jeff.burkholder@gmail.com>
2017-06-20 08:45:35 -0500

    complete clientside setup

---
+++ client/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+//import {TodoService} from './services/todo.service';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Angular warmly welcomes you!</h1>'
+  //templateUrl: 'app.component.html'//,
+//providers: [TodoService]
+})
+
+export class AppComponent { }
